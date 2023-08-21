4e24f65a4b8cb5da23ed64d4a3dc2f09fb37a22b
NikitaKx9 <nikitakx9@gmail.com>
2023-08-21 16:26:05 +0300

    style.css index.html changed

--- src/js/holeGame.js
+++ src/js/holeGame.js
@@ -10,10 +10,10 @@ holes.forEach((hole) => hole.addEventListener('click', () => {
   const getHole = (index) => document.getElementById(`hole${index}`);
   const deactivateHole = (index) => { getHole(index).className = 'hole'; };
   const activateHole = (index) => { 
-    console.log(getHole(index))
-    if (getHole(index).classList.contains('hole hole_has-goblin')) {
-      return
-    }
+    
+    // if (getHole(index).classList.contains('hole hole_has-goblin')) {
+    //   return
+    // }
     getHole(index).className = 'hole hole_has-goblin'; 
   };
   const next = () => setTimeout(() => {
@@ -21,7 +21,7 @@ holes.forEach((hole) => hole.addEventListener('click', () => {
     activeHole = Math.floor(1 + Math.random() * 16);
     activateHole(activeHole);
     next();
-  }, 800);
+  }, 1000);
 
   next();
 }
